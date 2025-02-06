61cd0a526cc707f0b6e686ad37f73d117dcf11d5
guuilhermessantos <guuilhermessantos@gmail.com>
2025-02-06 20:32:05 -0300

    fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://back-sendsafe01.vercel.app',
+  baseURL: 'https://back-sendsafe.onrender.com',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json'
